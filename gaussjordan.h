4a646c77253ecdf99c850770dd66ca629f204596
Mai Congyi 麦从一 <microboym@icloud.com>
2026-01-07 23:28:01 +0800

    Update .h

--- gaussjordan.h
+++ gaussjordan.h
@@ -8,6 +8,5 @@ void swap_rows(arr(arr(double)) Matrix, int n, int r1, int r2);
 int find_pivot(arr(arr(double)) Matrix, int start_row, int col, int n);
 void normalize_pivot_row(arr(arr(double)) Matrix, int row, int col, int n);
 void eliminate_rows(arr(arr(double)) Matrix, int pivot_row, int col, int n);
-int has_unique_solution(int n, arr(arr(double)) Matrix, int num_pivots);
 
 #endif
